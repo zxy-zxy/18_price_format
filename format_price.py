7359b58599c67696c73fdff7a5e8d2f9290ddacd
ivan <sinitsinvanya@gmail.com>
2018-06-08 14:05:17 +0400

    Initial commit

--- format_price.py
+++ format_price.py
@@ -1,5 +1,41 @@
+import argparse
+import sys
+from decimal import *
+
+
+def create_parser():
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        'number',
+        help='Number to format'
+    )
+
+    return parser
+
+
 def format_price(price):
-    pass
+    try:
+        price_in_decimal_format = Decimal(price)
+    except InvalidOperation:
+        raise ValueError
+
+    if price_in_decimal_format <= 0:
+        raise ValueError
+
+    return str.replace(
+        '{:,.2f}'.format(price_in_decimal_format),
+        ',',
+        ' '
+    )
+
 
 if __name__ == '__main__':
-    pass
+    args_parser = create_parser()
+    args = args_parser.parse_args()
+
+    try:
+        formatted_price = format_price(args.number)
+    except ValueError:
+        sys.exit('Invalid input.')
+
+    print(formatted_price)
