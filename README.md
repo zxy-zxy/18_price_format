c342bf0aaaac03bc7abc25e6a2885b90b2125fa3
ivan <sinitsinvanya@gmail.com>
2018-06-08 14:06:02 +0400

    Tests and readme updated

--- README.md
+++ README.md
@@ -1,7 +1,27 @@
 # Price Formatter
 
-[TODO. There will be project description]
+Python script to format price with a separation by thousands with a space.
 
-# Project Goals
-
+#Requirements
+Python 3.5 required.
+#Usage examples
+##Usage with terminal
+###Input
+```bash
+python format_price.py 10450.551
+``` 
+###Output
+```bash
+10 450.55
+```
+##Usage in your own python project 
+```python
+from format_price import format_price
+```
+#Tests
+To run and ensure tests are working 
+```bash
+python -m unittest tests.py
+```
+# Project goals
 The code is written for educational purposes. Training course for web-developers - [DEVMAN.org](https://devman.org)
--- tests.py
+++ tests.py
@@ -1,1 +1,25 @@
-# TODO place your tests here
+import unittest
+
+from format_price import format_price
+
+
+class FormatPriceTest(unittest.TestCase):
+    def test_thousand_integer_number(self):
+        self.assertTrue('5 340', format_price(5340))
+
+    def test_ten_thousand_integer_number(self):
+        self.assertTrue('10 450', format_price(10450))
+
+    def test_ten_thousand_float_number(self):
+        self.assertTrue('10 450.50', format_price(10450.5))
+
+    def test_float_number_with_long_decimal_part(self):
+        self.assertTrue('125 500', format_price(125500.001))
+
+    def test_negative_number_raises_value_error(self):
+        with self.assertRaises(ValueError):
+            format_price(-12500)
+
+    def test_string_input_raises_value_error(self):
+        with self.assertRaises(ValueError):
+            format_price('string input')
